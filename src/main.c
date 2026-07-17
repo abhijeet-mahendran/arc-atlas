ec4d93f5acfb77389ba2c8e6ce8793320f0dccb2
abhijeet-mahendran <abhijeetmahendran@googlemail.com>
2026-07-17 11:29:48 +0200

    Add initial project structure

---
+++ src/main.c
@@ -0,0 +1,15 @@
+// Ctrl + Shift + B to compile and run
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float height = 1.75f;
+    double GPA = 1.9;
+    char letter = 'A';
+
+    printf("%d\n", age);
+    printf("%f\n", height);
+    printf("%lf\n", GPA);
+    printf("%c\n", letter);
+    return 0; 
+}
